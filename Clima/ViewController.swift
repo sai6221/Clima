7bbcdbfcb6ecf01b1511057cbe27260531bfd2d9
Sai Reddy <sai6221@gmail.com>
2021-08-02 20:26:10 +0530

    Weather layout done

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate{
 
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempValue: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var textInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
