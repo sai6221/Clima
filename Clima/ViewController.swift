c485683ea0588ef0dd49078ad8843992d1880947
Sai Reddy <sai6221@gmail.com>
2021-08-02 23:37:44 +0530

    Basic input field designed

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -16,9 +16,32 @@ class ViewController: UIViewController,UITextFieldDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textInput.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchButton(_ sender: UIButton) {
+        textInput.endEditing(true)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textInput.endEditing(true)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if(textField.text != ""){
+            return true
+        }else{
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+//       use textField.text to city
+        textField.text = ""
+        
+    }
+    
 }
 
